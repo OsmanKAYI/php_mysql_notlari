2f32e88ccbfd79dde6664d225337b078fff5267b
Nuri Akman <nuriakman@gmail.com>
2023-01-26 16:39:03 +0300

    Update barkod.php

--- barcode/barkod.php
+++ barcode/barkod.php
@@ -6,6 +6,6 @@ require 'vendor/autoload.php';
 // This will output the barcode as HTML output to display in the browser
 $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
 echo '<p></p><img src="data:image/png;base64,' . base64_encode($generator->getBarcode('8682109342193', $generator::TYPE_CODE_128)) . '"><br>8682109342193</p>';
-echo '<p></p><img src="data:image/png;base64,' . base64_encode($generator->getBarcode('4547741547752', $generator::TYPE_CODE_128)) . '"><br>8682109342193</p>';
-echo '<p></p><img src="data:image/png;base64,' . base64_encode($generator->getBarcode('8469877445227', $generator::TYPE_CODE_128)) . '"><br>8682109342193</p>';
+echo '<p></p><img src="data:image/png;base64,' . base64_encode($generator->getBarcode('4547741547752', $generator::TYPE_CODE_128)) . '"><br>4547741547752</p>';
+echo '<p></p><img src="data:image/png;base64,' . base64_encode($generator->getBarcode('8469877445227', $generator::TYPE_CODE_128)) . '"><br>8469877445227</p>';
 
