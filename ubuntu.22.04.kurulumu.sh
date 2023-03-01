72397e3fc5579b950538c8b684e232cf46459570
Nuri Akman <nuriakman@gmail.com>
2023-03-01 13:06:19 +0300

    Update ubuntu.22.04.kurulumu.sh

--- ubuntu.22.04.kurulumu.sh
+++ ubuntu.22.04.kurulumu.sh
@@ -62,7 +62,7 @@ sudo apt update
 
 
 ## PHP 8.1 Sürümünü kur
-sudo apt install php8.1-fpm php8.1-intl php8.1-imagick php8.1-dev php8.1-zip php8.1-curl php8.1-xmlrpc php8.1-sqlite3 php8.1-gd php8.1-mysql php8.1-mbstring php8.1-xml php8.1-pgsql libapache2-mod-php8.1 -y
+sudo apt install php8.1-fpm php8.1-intl php8.1-imagick php8.1-dev php8.1-zip php8.1-curl php8.1-xmlrpc php8.1-sqlite3 php8.1-gd php8.1-mysql php8.1-mbstring php8.1-xml php8.1-redis php8.1-pgsql libapache2-mod-php8.1 -y
 ## PHP composer paketini kur
 sudo apt install composer -y
 ## PHP'nin çalışmaya başlaması için Apache'yi yeniden başlat
