7c9b0ecada74cb9722d8669fc02ab9cf63f64cab
Nuri Akman <nuriakman@gmail.com>
2023-12-27 11:33:50 +0300

    Update db.php

--- ornekler/PDO_1/db.php
+++ ornekler/PDO_1/db.php
@@ -5,7 +5,7 @@ $password   = "root";
 $dbname     = "ORNEKLER";
 
 try {
-  $DB = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $DB = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
   // set the PDO error mode to exception
   $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully";
